7e716415d1ad1eb2ef404a0c58277dc32d9b30fe
nabee <nabee@10.12.15.45>
2022-11-18 10:22:25 +0530

    test

--- src/jUnitTestPackage/jUnitTestFunctions.java
+++ src/jUnitTestPackage/jUnitTestFunctions.java
@@ -1,29 +1,13 @@
 package jUnitTestPackage;
-import java.util.Scanner;
+
 
 public class jUnitTestFunctions {
-	static void AddNumbers() {
-		Scanner s = new Scanner(System.in);
-		int a = s.nextInt();
-		int b = s.nextInt();
+	public int AddNumbers(int a,int b) {
 		int ans=a+b;
-		System.out.println(ans);
+		return ans;
 	}
-	static void AddStrings() {
-		Scanner sc = new Scanner(System.in);
-		String a = sc.nextLine();
-		String b = sc.nextLine();
+	public String AddStrings(String a,String b) {
 		String ans=a+b;
-		System.out.println(ans);
-	}
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int x = s.nextInt();
-		if (x==1) {
-			AddNumbers();
-		}
-		else if(x==2){
-			AddStrings();
-		}
+		return ans;
 	}
 }
